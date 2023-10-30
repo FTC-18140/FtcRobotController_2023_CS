e6ebbb82879947d7b7c87d1f9309a81735171094
bacon_101 <bacon.101@icloud.com>
2023-10-29 22:25:38 -0500

    Looking into code for custom vision pipeline to find the team game element.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CustomVisionProcessor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CustomVisionProcessor.java
@@ -7,12 +7,30 @@ import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibra
 import org.firstinspires.ftc.vision.VisionProcessor;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.core.Point;
+import org.opencv.imgproc.Moments;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomVisionProcessor implements VisionProcessor
 {
+
+    //Outputs
+    private Mat cvCvtcolorOutput = new Mat();
+    private Mat cvExtractchannelOutput = new Mat();
+    private Mat cvThresholdOutput = new Mat();
+    private Mat cvDilate0Output = new Mat();
+    private Mat cvErodeOutput = new Mat();
+    private Mat cvDilate1Output = new Mat();
+    private ArrayList<MatOfPoint> findContoursOutput = new ArrayList<MatOfPoint>();
+
+
     Mat smallMat = new Mat();
     Mat threshMatLow = new Mat();
     Mat threshMatHigh = new Mat();
@@ -40,13 +58,74 @@ public class CustomVisionProcessor implements VisionProcessor
     @Override
     public Object processFrame(Mat frame, long captureTimeNanos)
     {
-        Imgproc.cvtColor(frame, smallMat, Imgproc.COLOR_RGB2HSV);
-        Core.inRange(smallMat, lowerBoundLow, upperBoundLow, threshMatLow);
-        Core.inRange(smallMat, lowerBoundHigh, upperBoundHigh, threshMatHigh);
-        Core.add(threshMatHigh, threshMatLow, threshMatTot);
-        Imgproc.erode( threshMatTot, erodeMat, kernel);
-        Imgproc.dilate( erodeMat, dilateMat, kernel);
-        Imgproc.Laplacian(dilateMat, laplacianMat, 1);
+        // Step CV_cvtColor0:
+        Mat cvCvtcolorSrc = frame;
+        int cvCvtcolorCode = Imgproc.COLOR_RGB2YCrCb;
+        cvCvtcolor(cvCvtcolorSrc, cvCvtcolorCode, cvCvtcolorOutput);
+
+        // Step CV_extractChannel0:
+        Mat cvExtractchannelSrc = cvCvtcolorOutput;
+        double cvExtractchannelChannel = 1.0;
+        cvExtractchannel(cvExtractchannelSrc, cvExtractchannelChannel, cvExtractchannelOutput);
+
+        // Step CV_Threshold0:
+        Mat cvThresholdSrc = cvExtractchannelOutput;
+        double cvThresholdThresh = 140.0;
+        double cvThresholdMaxval = 255.0;
+        int cvThresholdType = Imgproc.THRESH_BINARY;
+        cvThreshold(cvThresholdSrc, cvThresholdThresh, cvThresholdMaxval, cvThresholdType, cvThresholdOutput);
+
+        // Step CV_dilate0:
+        Mat cvDilate0Src = cvThresholdOutput;
+        Mat cvDilate0Kernel = new Mat();
+        Point cvDilate0Anchor = new Point(-1, -1);
+        double cvDilate0Iterations = 10.0;
+        int cvDilate0Bordertype = Core.BORDER_CONSTANT;
+        Scalar cvDilate0Bordervalue = new Scalar(-1);
+        cvDilate(cvDilate0Src, cvDilate0Kernel, cvDilate0Anchor, cvDilate0Iterations, cvDilate0Bordertype, cvDilate0Bordervalue, cvDilate0Output);
+
+        // Step CV_erode0:
+        Mat cvErodeSrc = cvDilate0Output;
+        Mat cvErodeKernel = new Mat();
+        Point cvErodeAnchor = new Point(-1, -1);
+        double cvErodeIterations = 70.0;
+        int cvErodeBordertype = Core.BORDER_CONSTANT;
+        Scalar cvErodeBordervalue = new Scalar(-1);
+        cvErode(cvErodeSrc, cvErodeKernel, cvErodeAnchor, cvErodeIterations, cvErodeBordertype, cvErodeBordervalue, cvErodeOutput);
+
+        // Step CV_dilate1:
+        Mat cvDilate1Src = cvErodeOutput;
+        Mat cvDilate1Kernel = new Mat();
+        Point cvDilate1Anchor = new Point(-1, -1);
+        double cvDilate1Iterations = 70.0;
+        int cvDilate1Bordertype = Core.BORDER_CONSTANT;
+        Scalar cvDilate1Bordervalue = new Scalar(-1);
+        cvDilate(cvDilate1Src, cvDilate1Kernel, cvDilate1Anchor, cvDilate1Iterations, cvDilate1Bordertype, cvDilate1Bordervalue, cvDilate1Output);
+
+        // Step Find_Contours0:
+        Mat findContoursInput = cvDilate1Output;
+        boolean findContoursExternalOnly = false;
+        findContours(findContoursInput, findContoursExternalOnly, findContoursOutput);
+
+        if ( findContoursOutput.size() > 0 )
+        {
+            double maxArea = 0;
+            int maxIndex = 0;
+
+            for ( int i = 0; i < findContoursOutput.size(); i++ )
+            {
+                double currentArea = Imgproc.contourArea( findContoursOutput.get(i) );
+                if ( currentArea > maxArea)
+                {
+                    maxArea = currentArea;
+                    maxIndex = i;
+                }
+            }
+            Moments moments = Imgproc.moments(findContoursOutput.get(maxIndex));
+
+
+
+        }
 
         return laplacianMat;
     }
@@ -56,6 +135,167 @@ public class CustomVisionProcessor implements VisionProcessor
     {
 
     }
+
+    /**
+     * This method is a generated getter for the output of a CV_cvtColor.
+     * @return Mat output from CV_cvtColor.
+     */
+    public Mat cvCvtcolorOutput() {
+        return cvCvtcolorOutput;
+    }
+
+    /**
+     * This method is a generated getter for the output of a CV_extractChannel.
+     * @return Mat output from CV_extractChannel.
+     */
+    public Mat cvExtractchannelOutput() {
+        return cvExtractchannelOutput;
+    }
+
+    /**
+     * This method is a generated getter for the output of a CV_Threshold.
+     * @return Mat output from CV_Threshold.
+     */
+    public Mat cvThresholdOutput() {
+        return cvThresholdOutput;
+    }
+
+    /**
+     * This method is a generated getter for the output of a CV_dilate.
+     * @return Mat output from CV_dilate.
+     */
+    public Mat cvDilate0Output() {
+        return cvDilate0Output;
+    }
+
+    /**
+     * This method is a generated getter for the output of a CV_erode.
+     * @return Mat output from CV_erode.
+     */
+    public Mat cvErodeOutput() {
+        return cvErodeOutput;
+    }
+
+    /**
+     * This method is a generated getter for the output of a CV_dilate.
+     * @return Mat output from CV_dilate.
+     */
+    public Mat cvDilate1Output() {
+        return cvDilate1Output;
+    }
+
+    /**
+     * This method is a generated getter for the output of a Find_Contours.
+     * @return ArrayList<MatOfPoint> output from Find_Contours.
+     */
+    public ArrayList<MatOfPoint> findContoursOutput() {
+        return findContoursOutput;
+    }
+
+
+    /**
+     * Converts an image from one color space to another.
+     * @param src Image to convert.
+     * @param code conversion code.
+     * @param dst converted Image.
+     */
+    private void cvCvtcolor(Mat src, int code, Mat dst) {
+        Imgproc.cvtColor(src, dst, code);
+    }
+
+    /**
+     * Extracts given channel from an image.
+     * @param src the image to extract.
+     * @param channel zero indexed channel number to extract.
+     * @param dst output image.
+     */
+    private void cvExtractchannel(Mat src, double channel, Mat dst) {
+        Core.extractChannel(src, dst, (int)channel);
+    }
+
+    /**
+     * Apply a fixed-level threshold to each array element in an image.
+     * @param src Image to threshold.
+     * @param threshold threshold value.
+     * @param maxVal Maximum value for THRES_BINARY and THRES_BINARY_INV
+     * @param type Type of threshold to appy.
+     * @param dst output Image.
+     */
+    private void cvThreshold(Mat src, double threshold, double maxVal, int type,
+                             Mat dst) {
+        Imgproc.threshold(src, dst, threshold, maxVal, type);
+    }
+
+    /**
+     * Expands area of lower value in an image.
+     * @param src the Image to erode.
+     * @param kernel the kernel for erosion.
+     * @param anchor the center of the kernel.
+     * @param iterations the number of times to perform the erosion.
+     * @param borderType pixel extrapolation method.
+     * @param borderValue value to be used for a constant border.
+     * @param dst Output Image.
+     */
+    private void cvErode(Mat src, Mat kernel, Point anchor, double iterations,
+                         int borderType, Scalar borderValue, Mat dst) {
+        if (kernel == null) {
+            kernel = new Mat();
+        }
+        if (anchor == null) {
+            anchor = new Point(-1,-1);
+        }
+        if (borderValue == null) {
+            borderValue = new Scalar(-1);
+        }
+        Imgproc.erode(src, dst, kernel, anchor, (int)iterations, borderType, borderValue);
+    }
+
+    /**
+     * Expands area of higher value in an image.
+     * @param src the Image to dilate.
+     * @param kernel the kernel for dilation.
+     * @param anchor the center of the kernel.
+     * @param iterations the number of times to perform the dilation.
+     * @param borderType pixel extrapolation method.
+     * @param borderValue value to be used for a constant border.
+     * @param dst Output Image.
+     */
+    private void cvDilate(Mat src, Mat kernel, Point anchor, double iterations,
+                          int borderType, Scalar borderValue, Mat dst) {
+        if (kernel == null) {
+            kernel = new Mat();
+        }
+        if (anchor == null) {
+            anchor = new Point(-1,-1);
+        }
+        if (borderValue == null){
+            borderValue = new Scalar(-1);
+        }
+        Imgproc.dilate(src, dst, kernel, anchor, (int)iterations, borderType, borderValue);
+    }
+
+    /**
+     * Sets the values of pixels in a binary image to their distance to the nearest black pixel.
+     * @param input The image on which to perform the Distance Transform.
+     * @param type The Transform.
+     * @param maskSize the size of the mask.
+     * @param output The image in which to store the output.
+     */
+    private void findContours(Mat input, boolean externalOnly,
+                              List<MatOfPoint> contours) {
+        Mat hierarchy = new Mat();
+        contours.clear();
+        int mode;
+        if (externalOnly) {
+            mode = Imgproc.RETR_EXTERNAL;
+        }
+        else {
+            mode = Imgproc.RETR_LIST;
+        }
+        int method = Imgproc.CHAIN_APPROX_SIMPLE;
+        Imgproc.findContours(input, contours, hierarchy, mode, method);
+    }
+
 
     public static class Builder {
         public CustomVisionProcessor build() {

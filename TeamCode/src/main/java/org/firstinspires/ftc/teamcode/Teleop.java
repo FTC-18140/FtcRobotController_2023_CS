6bcfcd6e1200fe4948cafa6ef4016ac68c5b40d3
Aiden Fair <aiden.b.fair68@gmail.com>
2023-12-02 08:36:15 -0600

    Incorporated new tbdgamepad into teleop class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 public class Teleop extends OpMode  {
 
     Thunderbot2023 robot = new Thunderbot2023();
-
+    TBDGamepad gp1= new TBDGamepad(gamepad1);
     @Override
     public void init() {
         telemetry.addData("Init", "Start");
@@ -15,7 +15,6 @@ public class Teleop extends OpMode  {
         robot.init(hardwareMap, telemetry, false);
 
         telemetry.addData("Init", "Done");
-
     }
 
     @Override
@@ -33,20 +32,19 @@ public class Teleop extends OpMode  {
         //////////////
         // DRIVING
         //////////////
-        if (gamepad1.left_stick_button && gamepad1.right_stick_button) {
+        if (gp1.getButton(TBDGamepad.Button.LEFT_STICK_BUTTON) && gp1.getButton(TBDGamepad.Button.RIGHT_STICK_BUTTON)) {
             robot.imu.resetYaw();
             telemetry.addData("imu: ", "reset");
         }
 
-            if (gamepad1.right_trigger > 0.5) {
-                //TURBO
-                robot.orientedDrive(-gamepad1.left_stick_y * 0.9, -gamepad1.left_stick_x * 0.9, gamepad1.right_stick_x);
-            } else if (gamepad1.left_trigger > 0.5) {
-                //ACCURATE
-                robot.orientedDrive(-gamepad1.left_stick_y * 0.2, -gamepad1.left_stick_x * 0.2, gamepad1.right_stick_x);
-            } else {
-                //NORMAL
-                robot.orientedDrive(-gamepad1.left_stick_y * 0.6, -gamepad1.left_stick_x * 0.6, gamepad1.right_stick_x);
+        if (gp1.getTrigger(TBDGamepad.Trigger.LEFT_TRIGGER) > 0.5) {
+            //Expo
+            robot.orientedDrive(gp1.getExpo(TBDGamepad.Stick.LEFT_Y), gp1.getExpo(TBDGamepad.Stick.LEFT_X), gp1.getRightX());
+        } else if (gp1.getButton(TBDGamepad.Button.A)){
+            //Oriented
+            robot.orientedDrive(gp1.getLeftY() * 0.6, gp1.getLeftX() * 0.6, gp1.getRightX());
+        } else {
+            robot.joystickDrive(gp1.getExpo(TBDGamepad.Stick.LEFT_Y), gp1.getExpo(TBDGamepad.Stick.LEFT_X), gp1.getRightX());
         }
     }
 }

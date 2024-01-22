dd9a0c347d21915792d4f4993280c2f90ec62e9c
Aiden Fair <aiden.b.fair68@gmail.com>
2024-01-21 18:39:25 -0600

    Imported sensor class into here and started something for the beam break sensors as a touch sensor. will also code something else for a digital input.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Sensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Sensors.java
@@ -1,29 +1,91 @@
 package org.firstinspires.ftc.teamcode.Robot;
 
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-public class Sensors {
-    Telemetry  telemetry;
-    DistanceSensor dSensor = null;
-    ColorSensor cSensor = null;
+@TeleOp
+public class Sensors extends OpMode {
 
-    public void init(HardwareMap hwMap, Telemetry telem)
-    {
-        telemetry = telem;
-        try {
-            dSensor = hwMap.get(DistanceSensor.class, "distance");
-        } catch(Exception e) {
-            telemetry.addData("Distance Sensor not found", 0);
+    // Define a variable for our sensors
+    Rev2mDistanceSensor distanceLeft;
+    Rev2mDistanceSensor distanceRight;
+    ColorSensor colorright;
+    ColorSensor colorleft;
+    TouchSensor beamBreakLeft;
+    TouchSensor beamBreakRight;
+
+    int colorRed;
+    int colorBlue;
+
+    double distance;
+
+    @Override
+    public void init () {
+        // TODO: fix these comments to be the correct i2c busses
+
+        //I2C 2
+        distanceLeft = hardwareMap.get(Rev2mDistanceSensor.class, "distanceleft");
+        //I2C 1
+        distanceRight = hardwareMap.get(Rev2mDistanceSensor.class, "distanceright");
+        //I2C 3 Control Hub
+        colorright = hardwareMap.get(ColorSensor.class, "colorright");
+        //I2C 3
+        colorleft = hardwareMap.get(ColorSensor.class, "colorleft");
+        // TODO: Digital ?
+        beamBreakLeft = hardwareMap.get(TouchSensor.class, "bbrleft");
+        // TODO: Digital ?
+        beamBreakRight = hardwareMap.get(TouchSensor.class, "bbrright");
+
+    }
+
+    @Override
+    public void loop() {
+//        telemetry.addData("Distance1", distanceLeft.getDistance(DistanceUnit.CM));
+//        telemetry.addData("Distance2", distanceRight.getDistance(DistanceUnit.CM));
+//        telemetry.addData("Left Blue ColorV3", colorleft.blue());
+//        telemetry.addData("Right Blue ColorV3", colorright.blue());
+//        telemetry.addData("Left Red ColorV3", colorleft.red());
+//        telemetry.addData("Right Red ColorV3", colorright.red());
+        telemetry.addData("Tape Color", TapeLineColor());
+        telemetry.addData("Backdrop distance", GetAverageDistance());
+        telemetry.addData("Beam Break Status Left", beamBreakLeft.isPressed());
+        telemetry.addData("Beam Break Status Right", beamBreakRight.isPressed());
+        telemetry.update();
+
+        colorBlue = ((colorleft.blue() + colorright.blue()) / 2);
+        colorRed = ((colorleft.red() + colorright.red()) / 2);
+
+        distance = ((distanceLeft.getDistance(DistanceUnit.CM) + distanceRight.getDistance(DistanceUnit.CM)) / 2);
+    }
+
+    public int TapeLineColor () {
+
+        // 0 = can't tell
+        // 1 = blue
+        // 2 = red
+
+        int tapeColor = 0;
+
+        if (colorBlue > (colorRed + 250)) {
+            tapeColor = 1;
+
+        } else if (colorRed > (colorBlue + 50)) {
+            tapeColor = 2;
+
         }
-        try {
-            cSensor = hwMap.colorSensor.get("color");
-        } catch(Exception e) {
-            telemetry.addData("Color Sensor not found", 0);
-        }
+
+        return tapeColor;
+    }
+
+
+    public double GetAverageDistance () {
+        return distance;
     }
 
 }
+

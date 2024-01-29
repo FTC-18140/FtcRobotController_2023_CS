ac33d54604c7ca3429787e7cd8b4954cfe388d14
River <jeremy.gbsn@gmail.com>
2024-01-29 15:48:43 -0600

    Async Work

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRAutoRedR.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+@Autonomous
+public class RRAutoRedR extends OpMode {
+    SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+    final int START_X = -58;
+    final int START_Y = -12;
+
+    final int END_X = -58;
+    final int END_Y = -58;
+
+    boolean done = false;
+
+    Trajectory step1;
+    //Thunderbot2023 robot = new Thunderbot2023();
+
+    @Override
+    public void init(){
+        //robot.init(hardwareMap, telemetry, true);
+        //0.9083333
+    }
+    @Override
+    public void start(){
+        step1 = drive.trajectoryBuilder(new Pose2d(START_X, START_Y, Math.toRadians(0)))
+                .strafeTo(new Vector2d(END_X, END_Y))
+                .build();
+        drive.followTrajectoryAsync(step1);
+    }
+    @Override
+    public void loop(){
+        drive.update();
+        //robot.update();
+
+    }
+}

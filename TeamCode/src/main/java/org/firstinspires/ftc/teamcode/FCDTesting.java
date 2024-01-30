5b1fb29c3143e1bdbc203d0f79045e8716a7880b
Aiden Fair <aiden.b.fair68@gmail.com>
2024-01-30 16:14:54 -0600

    FCD is working!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FCDTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FCDTesting.java
@@ -11,7 +11,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
-@TeleOp
+@TeleOp (name = "FCD", group = "Teleop")
 public class FCDTesting extends OpMode {
 
     Thunderbot2023 robot = new Thunderbot2023();

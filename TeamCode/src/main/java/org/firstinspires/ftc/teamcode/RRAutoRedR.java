2381b2664a83ed03be1506c5481988eec05b45e9
River <jeremy.gbsn@gmail.com>
2024-01-31 17:08:39 -0600

    Plotting out trajectories

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRAutoRedR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRAutoRedR.java
@@ -12,11 +12,40 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 public class RRAutoRedR extends OpMode {
 
     SampleMecanumDrive drive;
+
+    int spike = 1;
     final int START_X = -58;
     final int START_Y = -12;
 
     final int END_X = -58;
     final int END_Y = -58;
+
+    final int SPIKE_L_X = -28;
+    final int SPIKE_M_X = -24;
+    final int SPIKE_R_X = -28;
+
+    final int SPIKE_L_Y = 0;
+    final int SPIKE_M_Y = -12;
+    final int SPIKE_R_Y = -24;
+
+    int spike_x;
+    int spike_y;
+
+    final int BACKDROP_L_X = -30;
+    final int BACKDROP_M_X = -36;
+    final int BACKDROP_R_X = -42;
+
+    final int BACKDROP_L_Y = -48;
+    final int BACKDROP_M_Y = -48;
+    final int BACKDROP_R_Y = -48;
+
+    int backdrop_x;
+    int backdrop_y;
+
+    final int TRUSS_IN_X = -36;
+    final int TRUSS_IN_Y = -2;
+    final int TRUSS_OUT_X = -36;
+    final int TRUSS_OUT_Y = 26;
 
     boolean done = false;
 
@@ -36,7 +65,12 @@ public class RRAutoRedR extends OpMode {
     *  place pixel
     * PARK
     * */
-    Trajectory step1;
+
+    Trajectory purple;
+    Trajectory yellow;
+
+    Trajectory truss1;
+    Trajectory park;
     //Thunderbot2023 robot = new Thunderbot2023();
 
     @Override
@@ -47,11 +81,46 @@ public class RRAutoRedR extends OpMode {
     }
     @Override
     public void start(){
+        switch(spike){
+            case(1):
+                spike_x = SPIKE_L_X;
+                spike_y = SPIKE_L_Y;
+                backdrop_x = BACKDROP_L_X;
+                backdrop_y = BACKDROP_L_Y;
+                break;
+            case(2):
+                spike_x = SPIKE_M_X;
+                spike_y = SPIKE_M_Y;
+                backdrop_x = BACKDROP_M_X;
+                backdrop_y = BACKDROP_M_Y;
+                break;
+            case(3):
+                spike_x = SPIKE_R_X;
+                spike_y = SPIKE_R_Y;
+                backdrop_x = BACKDROP_R_X;
+                backdrop_y = BACKDROP_R_Y;
+                break;
 
-        step1 = drive.trajectoryBuilder(new Pose2d(START_X, START_Y, Math.toRadians(0)))
+
+        }
+        purple = drive.trajectoryBuilder(new Pose2d(START_X ,START_Y, Math.toRadians(0)))
+                .strafeTo(new Vector2d(spike_x, spike_y))
+                .build();
+
+        yellow = drive.trajectoryBuilder(purple.end())
+                .lineToSplineHeading(new Pose2d(backdrop_x, backdrop_y, Math.toRadians(-90)))
+                .build();
+
+        truss1 = drive.trajectoryBuilder(yellow.end())
+                .splineToSplineHeading(new Pose2d(TRUSS_IN_X, TRUSS_IN_Y, Math.toRadians(90)), Math.toRadians(180))
+                .splineToConstantHeading(new Vector2d(TRUSS_OUT_X, TRUSS_OUT_Y), Math.toRadians(0))
+                .build();
+
+        park = drive.trajectoryBuilder(new Pose2d(START_X, START_Y, Math.toRadians(0)))
                 .strafeTo(new Vector2d(END_X, END_Y))
                 .build();
-        drive.followTrajectoryAsync(step1);
+
+        drive.followTrajectoryAsync(park);
     }
     @Override
     public void loop(){

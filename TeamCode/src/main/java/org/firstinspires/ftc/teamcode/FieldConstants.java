543ec09c5b5ac242c9509c4d2444ec43ad4d1682
River <jeremy.gbsn@gmail.com>
2024-02-23 08:31:38 -0600

    Adjusting Y Multiplier

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldConstants.java
@@ -8,14 +8,14 @@ public class FieldConstants {
         START(12,-63, Math.toRadians(90)),
         SPIKE_LEFT(10,-36,Math.toRadians(140)),
         SPIKE_CENTER(23,-32.5,Math.toRadians(120)),
-        SPIKE_RIGHT(26,-40,Math.toRadians(100)),
+        SPIKE_RIGHT(25,-40,Math.toRadians(100)),
         BACKDROP_LEFT(47,-33.5,90),
         BACKDROP_CENTER(47,-40.5,90),
         BACKDROP_RIGHT(47,-46.5,Math.toRadians(179.9)),
         TRUSS_IN(-36,-12,Math.toRadians(90)),
         TRUSS_OUT(-36,36,Math.toRadians(90)),
         STACK(-24,52,Math.toRadians(90)),
-        PARK(54,-62,Math.toRadians(0)),
+        PARK(54,-60,Math.toRadians(0)),
         PARK_LEFT(54,-10,Math.toRadians(0));
 
         public final double x;
